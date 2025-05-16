07131b2222ec2c831340070538c64cdba8f102b7
DharsshneRajavel <dharsshne16@gmail.com>
2025-05-16 17:54:35 +0530

    Completed frontend assessment tasks

--- TOPIC 1/SCRIPTS/script.js
+++ TOPIC 1/SCRIPTS/script.js
@@ -1,2 +1,8 @@
+function displayMessage() {
+    console.log("Button clicked! Welcome to the Core Trio page.");
+    alert("Hello from JavaScript!");
+}
+
+console.log("Page loaded successfully!");
 
 
